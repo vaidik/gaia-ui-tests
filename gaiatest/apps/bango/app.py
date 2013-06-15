42066aee063121646d44f6c6d102abbf760b4843
zacc <zcampbell@mozilla.com>
2013-06-15 20:40:57 +0100

    Rename test_marketplace_purchase_app_wifi.py and bango.py to app.py

--- gaiatest/apps/bango/app.py
+++ gaiatest/apps/bango/app.py
@@ -103,7 +103,8 @@ class Bango(Base):
 
         # TODO Re-enable this when Bug 861874
         # self.wait_for_element_displayed(*self._notification_toaster_locator)
-        self.wait_for_condition(lambda m: notification_toaster.location['y'] == 0, timeout=180)
+        self.wait_for_condition(lambda m: notification_toaster.location['y'] == 0,
+                                timeout=180, message="SMS was not received before 180 second timeout")
 
         m = re.search("PIN: ([0-9]+).", notification_toaster.text)
         pin_number = m.group(1)
